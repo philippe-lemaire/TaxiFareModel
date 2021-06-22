b4e5f77e047ec656f617b23a65461447cebebdeb
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-06-22 15:31:40 +0200

    with comments

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -11,13 +11,26 @@ import mlflow
 from mlflow.tracking import MlflowClient
 from memoized_property import memoized_property
 import joblib
+import xgboost as xgb
 
 # Local imports
 from TaxiFareModel import data, utils
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 
 MLFLOW_URI = "https://mlflow.lewagon.co/"
-EXPERIMENT_NAME = "[FR] [lyon] [philippe-lemaire] linreg 2"
+EXPERIMENT_NAME = "[FR] [lyon] [philippe-lemaire] TaxiFare"
+
+params = dict(
+    nrows=100_000,  # number of samples
+    local=False,  # get data from AWS
+    optimize=True,
+    estimator=LinearRegression(),
+    mlflow=True,  # set to True to log params to mlflow
+    experiment_name=EXPERIMENT_NAME,
+    pipeline_memory=None,
+    distance_type="manhattan",
+    feateng=["distance_to_center", "direction", "distance", "time_features", "geohash"],
+)
 
 
 class Trainer:
@@ -64,7 +77,7 @@ class Trainer:
 
         # Add the model of your choice to the pipeline
         self.pipeline = Pipeline(
-            [("preproc", preproc_pipe), ("linear_model", LinearRegression())]
+            [("preproc", preproc_pipe), ("linear_model", params["estimator"])]
         )
 
     def run(self):
@@ -75,7 +88,7 @@ class Trainer:
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
-        print(utils.compute_rmse(y_pred, y_test))
+        return utils.compute_rmse(y_pred, y_test)
 
     @memoized_property
     def mlflow_client(self):
@@ -107,14 +120,25 @@ class Trainer:
 
 
 if __name__ == "__main__":
-
-    df = data.get_data()
+    # get data
+    df = data.get_data(nrows=params["nrows"])
     df = data.clean_data(df)
     y = df.pop("fare_amount")
     X = df
+    # holdout
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+    # init trainer
     trainer = Trainer(X_train, y_train)
+    # set pipeline and train
     trainer.run()
-    # evaluate
-    trainer.evaluate(X_val, y_val)
+    # log params
+    trainer.mlflow_log_param("nrows", params["nrows"])
+    # print and log eval
+    eval = trainer.evaluate(X_val, y_val)
+    print(f"RMSE results: {eval}")
+    trainer.mlflow_log_metric("rmse", eval)
+    # print experiment's link
+    experiment_id = trainer.mlflow_experiment_id
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
+    # save model
     trainer.save_model()
